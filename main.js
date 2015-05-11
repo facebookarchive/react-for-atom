c084eb128d281a09298e57be28526437e709f2c7
Jonas Gebhardt <jonas@instagram.com>
2015-05-11 11:33:28 -0700

    Inject custom DOM config

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
-var atom = global && global.atom || null;
 var React;
+
+function _injectCustomConfig() {
+  var ReactInjection = require('react/lib/ReactInjection');
+  var customAttributes = {
+    mini: ReactInjection.DOMProperty.HAS_BOOLEAN_VALUE,
+  };
+  var AtomDOMPropertyConfig = {
+    isCustomAttribute: function(attributeName) {
+      return customAttributes[attributeName] !== undefined;
+    },
+    Properties: customAttributes,
+  };
+  ReactInjection.DOMProperty.injectDOMPropertyConfig(AtomDOMPropertyConfig);
+}
 
 if (typeof atom === 'object' && atom !== null) {
   if (atom.__DO_NOT_ACCESS_React_Singleton === undefined) {
     React = require('react');
+    _injectCustomConfig();
     var addons;
     Object.defineProperty(React, 'addons', {
       get() {
