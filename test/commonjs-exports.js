d8f047faf1032e05ff890de75adcc23783cd35e4
Andres Suarez <zertosh@gmail.com>
2016-05-23 11:23:58 -0700

    Export minified React when NODE_ENV is "production"

--- test/commonjs-exports.js
+++ test/commonjs-exports.js
@@ -7,18 +7,42 @@ var test = require('tap').test;
 var ReactForAtom = require('../');
 
 test('react-for-atom commonjs exports', function (t) {
-  t.plan(13);
-  t.equal(ReactForAtom.default, require('react'));
-  t.equal(ReactForAtom.createFragment, require('react-addons-create-fragment'));
-  t.equal(ReactForAtom.CSSTransitionGroup, require('react-addons-css-transition-group'));
-  t.equal(ReactForAtom.LinkedStateMixin, require('react-addons-linked-state-mixin'));
-  t.equal(ReactForAtom.Perf, require('react-addons-perf'));
-  t.equal(ReactForAtom.PureRenderMixin, require('react-addons-pure-render-mixin'));
-  t.equal(ReactForAtom.React, require('react'));
-  t.equal(ReactForAtom.ReactDOM, require('react-dom'));
-  t.equal(ReactForAtom.ReactDOMServer, require('react-dom/server'));
-  t.equal(ReactForAtom.shallowCompare, require('react-addons-shallow-compare'));
-  t.equal(ReactForAtom.TestUtils, require('react-addons-test-utils'));
-  t.equal(ReactForAtom.TransitionGroup, require('react-addons-transition-group'));
-  t.equal(ReactForAtom.update, require('react-addons-update'));
+  t.plan(15);
+
+  if (process.env.NODE_ENV !== 'production') {
+    t.equal(ReactForAtom.default, require('react'));
+    t.equal(ReactForAtom.createFragment, require('react-addons-create-fragment'));
+    t.equal(ReactForAtom.CSSTransitionGroup, require('react-addons-css-transition-group'));
+    t.equal(ReactForAtom.LinkedStateMixin, require('react-addons-linked-state-mixin'));
+    t.equal(ReactForAtom.Perf, require('react-addons-perf'));
+    t.equal(ReactForAtom.PureRenderMixin, require('react-addons-pure-render-mixin'));
+    t.equal(ReactForAtom.React, require('react'));
+    t.equal(ReactForAtom.ReactDOM, require('react-dom'));
+    t.equal(ReactForAtom.ReactDOMServer, require('react-dom/server'));
+    t.equal(ReactForAtom.shallowCompare, require('react-addons-shallow-compare'));
+    t.equal(ReactForAtom.TestUtils, require('react-addons-test-utils'));
+    t.equal(ReactForAtom.TransitionGroup, require('react-addons-transition-group'));
+    t.equal(ReactForAtom.update, require('react-addons-update'));
+    t.equal(Object.keys(ReactForAtom).length, 13);
+  } else {
+    t.equal(ReactForAtom.default, require('react/dist/react-with-addons.min'));
+    t.equal(ReactForAtom.createFragment, require('react/dist/react-with-addons.min').addons.createFragment);
+    t.equal(ReactForAtom.CSSTransitionGroup, require('react/dist/react-with-addons.min').addons.CSSTransitionGroup);
+    t.equal(ReactForAtom.LinkedStateMixin, require('react/dist/react-with-addons.min').addons.LinkedStateMixin);
+    t.notOk('Perf' in ReactForAtom);
+    t.equal(ReactForAtom.PureRenderMixin, require('react/dist/react-with-addons.min').addons.PureRenderMixin);
+    t.equal(ReactForAtom.React, require('react/dist/react-with-addons.min'));
+    t.equal(ReactForAtom.ReactDOM, require('react/dist/react-with-addons.min').__SECRET_DOM_DO_NOT_USE_OR_YOU_WILL_BE_FIRED);
+    t.equal(ReactForAtom.ReactDOMServer, require('react/dist/react-with-addons.min').__SECRET_DOM_SERVER_DO_NOT_USE_OR_YOU_WILL_BE_FIRED);
+    t.equal(ReactForAtom.shallowCompare, require('react/dist/react-with-addons.min').addons.shallowCompare);
+    t.notOk('TestUtils' in ReactForAtom);
+    t.equal(ReactForAtom.TransitionGroup, require('react/dist/react-with-addons.min').addons.TransitionGroup);
+    t.equal(ReactForAtom.update, require('react/dist/react-with-addons.min').addons.update);
+    t.equal(Object.keys(ReactForAtom).length, 11);
+  }
+
+  var everyExportHasAValue = Object.keys(ReactForAtom).every(function(key) {
+    return typeof ReactForAtom[key] !== 'undefined';
+  });
+  t.ok(everyExportHasAValue);
 });
--- test/es-module-named-exports.js
+++ test/es-module-named-exports.js
@@ -15,13 +15,13 @@ test('react-for-atom es-module named exports', function (t) {
     '  createFragment,',
     '  CSSTransitionGroup,',
     '  LinkedStateMixin,',
-    '  Perf,',
+    process.env.NODE_ENV !== 'production' ? '  Perf,' : '',
     '  PureRenderMixin,',
     '  React,',
     '  ReactDOM,',
     '  ReactDOMServer,',
     '  shallowCompare,',
-    '  TestUtils,',
+    process.env.NODE_ENV !== 'production' ? '  TestUtils,' : '' ,
     '  TransitionGroup,',
     '  update,',
     '} from "..";',
@@ -31,13 +31,13 @@ test('react-for-atom es-module named exports', function (t) {
     '  createFragment,',
     '  CSSTransitionGroup,',
     '  LinkedStateMixin,',
-    '  Perf,',
+    process.env.NODE_ENV !== 'production' ? '  Perf,' : '',
     '  PureRenderMixin,',
     '  React,',
     '  ReactDOM,',
     '  ReactDOMServer,',
     '  shallowCompare,',
-    '  TestUtils,',
+    process.env.NODE_ENV !== 'production' ? '  TestUtils,' : '',
     '  TransitionGroup,',
     '  update,',
     '};',
@@ -52,17 +52,34 @@ test('react-for-atom es-module named exports', function (t) {
   };
   vm.runInNewContext(code, c);
 
-  t.equal(c.module.exports.ReactDefault, require('react'));
-  t.equal(c.module.exports.createFragment, require('react-addons-create-fragment'));
-  t.equal(c.module.exports.CSSTransitionGroup, require('react-addons-css-transition-group'));
-  t.equal(c.module.exports.LinkedStateMixin, require('react-addons-linked-state-mixin'));
-  t.equal(c.module.exports.Perf, require('react-addons-perf'));
-  t.equal(c.module.exports.PureRenderMixin, require('react-addons-pure-render-mixin'));
-  t.equal(c.module.exports.React, require('react'));
-  t.equal(c.module.exports.ReactDOM, require('react-dom'));
-  t.equal(c.module.exports.ReactDOMServer, require('react-dom/server'));
-  t.equal(c.module.exports.shallowCompare, require('react-addons-shallow-compare'));
-  t.equal(c.module.exports.TestUtils, require('react-addons-test-utils'));
-  t.equal(c.module.exports.TransitionGroup, require('react-addons-transition-group'));
-  t.equal(c.module.exports.update, require('react-addons-update'));
+  if (process.env.NODE_ENV !== 'production') {
+    t.equal(c.module.exports.ReactDefault, require('react'));
+    t.equal(c.module.exports.createFragment, require('react-addons-create-fragment'));
+    t.equal(c.module.exports.CSSTransitionGroup, require('react-addons-css-transition-group'));
+    t.equal(c.module.exports.LinkedStateMixin, require('react-addons-linked-state-mixin'));
+    t.equal(c.module.exports.Perf, require('react-addons-perf'));
+    t.equal(c.module.exports.PureRenderMixin, require('react-addons-pure-render-mixin'));
+    t.equal(c.module.exports.React, require('react'));
+    t.equal(c.module.exports.ReactDOM, require('react-dom'));
+    t.equal(c.module.exports.ReactDOMServer, require('react-dom/server'));
+    t.equal(c.module.exports.shallowCompare, require('react-addons-shallow-compare'));
+    t.equal(c.module.exports.TestUtils, require('react-addons-test-utils'));
+    t.equal(c.module.exports.TransitionGroup, require('react-addons-transition-group'));
+    t.equal(c.module.exports.update, require('react-addons-update'));
+  } else {
+    t.equal(c.module.exports.ReactDefault, require('react/dist/react-with-addons.min'));
+    t.equal(c.module.exports.createFragment, require('react/dist/react-with-addons.min').addons.createFragment);
+    t.equal(c.module.exports.CSSTransitionGroup, require('react/dist/react-with-addons.min').addons.CSSTransitionGroup);
+    t.equal(c.module.exports.LinkedStateMixin, require('react/dist/react-with-addons.min').addons.LinkedStateMixin);
+    t.notOk('Perf' in c.module.exports);
+    t.equal(c.module.exports.PureRenderMixin, require('react/dist/react-with-addons.min').addons.PureRenderMixin);
+    t.equal(c.module.exports.React, require('react/dist/react-with-addons.min'));
+    t.equal(c.module.exports.ReactDOM, require('react/dist/react-with-addons.min').__SECRET_DOM_DO_NOT_USE_OR_YOU_WILL_BE_FIRED);
+    t.equal(c.module.exports.ReactDOMServer, require('react/dist/react-with-addons.min').__SECRET_DOM_SERVER_DO_NOT_USE_OR_YOU_WILL_BE_FIRED);
+    t.equal(c.module.exports.shallowCompare, require('react/dist/react-with-addons.min').addons.shallowCompare);
+    t.notOk('TestUtils' in c.module.exports);
+    t.equal(c.module.exports.TransitionGroup, require('react/dist/react-with-addons.min').addons.TransitionGroup);
+    t.equal(c.module.exports.update, require('react/dist/react-with-addons.min').addons.update);
+  }
+
 });
